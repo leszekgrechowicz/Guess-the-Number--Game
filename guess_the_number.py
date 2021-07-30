a2c43b0a6e54851bc42930660699afc0891b5860
Leszek Grechowicz <leszek_grechowicz@o2.pl>
2021-07-30 22:50:22 +0100

    Complete game

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,40 +2,65 @@
 """
 Author : Leszek Grechowicz <leszek_grechowicz@o2.pl>
 Date   : 29-07-2021
-Purpose: Terminal based Guess the number Game
+Purpose: Terminal based Guess the Number Game
 """
 
 from art.logo import logo
 from random import randint
 
 
-def check_guess():
-    """checking if players input is correct(a number), prompting to enter again if not"""
+def take_input() -> int:
+    """Asking for input checking if correct and prompting to enter again if not"""
     while True:
         try:
-            pick_num = int(input('\t>>> '))
+            pick_num = int(input('\n\tGuess the number: '))
             return pick_num
         except ValueError:
-            print("\tIt is not a number !, Please try again.")
+            print("\tThis is not a number ! Please try again.")
             continue
 
 
-def game():
+def check_guess(rand_num: int):
+    """Checking players number against random generated number"""
+    the_same = False
+    while not the_same:
+
+        # stores player guess
+        player_num = take_input()
+
+        if player_num == rand_num:
+            the_same = True
+
+        elif player_num > rand_num:
+            print("\tTo big!")
+            continue
+
+        else:
+            print('\tTo small!')
+            continue
+
+    print("\tCongratulations You win!")
+
+
+def play():
+    """Game play"""
     print("\n\tI am thinking about the number between 1 and 100, TRY ME !")
+
+    # stores random generated number for the player to guess:
     number_to_guess = randint(1, 100)
-    guess = check_guess()
-    pass
+
+    check_guess(number_to_guess)
 
 
-def main():
-    """game main loop, checking if player wants to play again"""
+def game():
+    """Main game loop, when game ends checks if player wants to play again"""
     print(logo)
     game_on = True
     while game_on:
-        game()
-        response = input("Do you want to play again? Y to CONTINUE or ANY key to QUIT >>> ")
+        play()
+        response = input("\nDo you want to play again? Y to CONTINUE or ANY key to QUIT >>> ")
         game_on = True if response.lower() == 'y' else False
 
 
 if __name__ == '__main__':
-    main()
+    game()
